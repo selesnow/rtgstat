a316d0e49d95c52f861d7f79ee061f8585d4b6f5
Alexey <selesnow@gmail.com>
2021-11-20 16:02:46 +0200

    В справку функции tg_channel_subscribers() добавлен блок details, с детальным описанием рабты.

--- R/tg_channel_subscribers.R
+++ R/tg_channel_subscribers.R
@@ -5,13 +5,24 @@
 #' @param end_date End date of report period
 #' @param group Time group: hour, day, week, month
 #'
+#' @details
+#' For grouping group = day, the number of subscribers as of the end of the day will be returned.
+#'
+#' For groupings group = week and group = month, the number of subscribers at the end of the last day of the period (week or month) will be returned.
+#'
+#' Depending on the requested grouping type group - the period field will take one of the following formats:
+#' * hour: Y-m-d H:00
+#' * day: Y-m-d
+#' * week: Y-W
+#' * month: Y-m
+#'
 #' @references See also \href{https://api.tgstat.ru/docs/ru/channels/subscribers.html}{TGstat API Documentation of metrod channels/subscribers}
 #'
 #' @return tibble with subscribers stat
 #' @export
 #' @examples
 #' \dontrun{
-#' channel_subscribers <- tg_channel(
+#' channel_subscribers <- tg_channel_subscribers(
 #'     channel_id = "R4marketing",
 #'     start_date = "2021-06-01",
 #'     end_date = "2021-10-31",
